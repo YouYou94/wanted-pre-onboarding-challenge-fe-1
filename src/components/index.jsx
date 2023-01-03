ef93768763affee677da6e029a7cc82ddefb1f40
YouYou94 <ybh4115@gmail.com>
2023-01-03 16:59:31 +0900

    export: Todo 컴포넌트 export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,6 @@ export { Layout } from './common/Layout.jsx';
 
 export { Login } from './Auth/Login.jsx';
 export { Sign } from './Auth/Sign.jsx';
+
+export { TodoHeader } from './Todo/TodoHeader/TodoHeader.jsx';
+export { TodoContent } from './Todo/TodoContent/TodoContent.jsx';
