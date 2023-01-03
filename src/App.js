cf48b2ed7a33a3e32009d501c4e305e0fc4877c5
YouYou94 <ybh4115@gmail.com>
2023-01-03 19:55:38 +0900

    api: 서버 연동 및 todolist 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { createContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import './Reset.css';
 import Router from './router/Router.jsx';
 
+export const TodoContext = createContext();
+
 function App() {
+  const [todolist, setTodolist] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getTodo() {
+      await fetch('http://localhost:8080/todos', {
+        method: 'GET',
+        headers: {
+          Authorization: localStorage.getItem('user'),
+        },
+      })
+        .then(res => res.json())
+        .then(data => {
+          setTodolist(data);
+        });
+
+      setIsLoading(false);
+    }
+    getTodo();
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <TodoContext.Provider value={todolist}>
+        {isLoading ? (
+          <></>
+        ) : (
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        )}
+      </TodoContext.Provider>
     </div>
   );
 }
