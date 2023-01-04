bfd75054f81c982b09268e8f1798a2ec667020c7
YouYou94 <ybh4115@gmail.com>
2023-01-04 21:20:34 +0900

    Feat: 투두리스트 추가 기능 구현, api 연동 후 todolist 데이터 받기 성공

--- src/components/Todo/List/List.jsx
+++ src/components/Todo/List/List.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { customAlphabet } from 'nanoid';
 import {
   ListContainer,
   ListHeader,
@@ -10,10 +11,62 @@ import {
   ContentArea,
   ModalButtonBox,
   ModalButton,
+  ListItem,
 } from './ListStyled';
 
 export function List() {
+  const loginToken = localStorage.getItem('user');
+  const nanoid = customAlphabet('0123456789abcedfghi', 8);
+
+  // 변경 사항 체크, 변경 사항이 일어날 시 useEffect 실행
+  const [change, setChange] = useState(0);
+
   const [isDisplay, setIsDisplay] = useState(false);
+  const [todoState, setTodoState] = useState({
+    title: '',
+    content: '',
+  });
+  const [list, setList] = useState([]);
+
+  // to do list 불러오기
+  const getTodo = async () => {
+    await fetch('http://localhost:8080/todos', {
+      method: 'GET',
+      headers: {
+        Authorization: loginToken,
+      },
+    })
+      .then(res => res.json())
+      .then(data => setList(data.data))
+      .catch(error => console.log(`Error: ${error}`));
+  };
+
+  // to do list 추가하기
+  const onHandlerClickCreate = async () => {
+    const { title, content } = todoState;
+
+    await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        content: content,
+      }),
+      headers: {
+        Authorization: loginToken,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .catch(error => console.log(error));
+
+    setIsDisplay(false);
+    setTodoState({ title: '', content: '' });
+    setChange(change + 1);
+  };
+
+  useEffect(() => {
+    getTodo();
+  }, [change]);
 
   return (
     <ListContainer>
@@ -25,14 +78,42 @@ export function List() {
       </ListHeader>
       <ListBox>
         <ModalBox isDisplay={isDisplay}>
-          <TitleInput placeholder="제목을 작성해주세요." />
-          <ContentArea placeholder="내용을 작성해주세요." />
+          <TitleInput
+            value={todoState.title}
+            onChange={event =>
+              setTodoState({ ...todoState, title: event.target.value })
+            }
+            placeholder="제목을 작성해주세요."
+          />
+          <ContentArea
+            value={todoState.content}
+            onChange={event =>
+              setTodoState({ ...todoState, content: event.target.value })
+            }
+            placeholder="내용을 작성해주세요."
+          />
           <ModalButtonBox>
-            <ModalButton>작성</ModalButton>
-            <ModalButton>초기화</ModalButton>
-            <ModalButton onClick={() => setIsDisplay(false)}>닫기</ModalButton>
+            <ModalButton onClick={onHandlerClickCreate}>추 가</ModalButton>
+            <ModalButton
+              onClick={() => setTodoState({ title: '', content: '' })}
+            >
+              초기화
+            </ModalButton>
+            <ModalButton
+              onClick={() => {
+                setIsDisplay(false);
+                setTodoState({ title: '', content: '' });
+              }}
+            >
+              닫 기
+            </ModalButton>
           </ModalButtonBox>
         </ModalBox>
+        {list.map(item => {
+          const { title } = item;
+
+          return <ListItem key={nanoid()}>{title}</ListItem>;
+        })}
       </ListBox>
     </ListContainer>
   );
