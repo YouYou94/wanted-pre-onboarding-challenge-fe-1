71a83b270dabbd1e1e5df69f827a93a0256f8cce
YouYou94 <ybh4115@gmail.com>
2023-01-04 21:29:55 +0900

    Mod: 투두리스트 정보 불러오기 삭제

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import { createContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Router from './router/Router.jsx';
 import './App.css';
 import './Reset.css';
-import Router from './router/Router.jsx';
-
-export const TodoContext = createContext();
 
 function App() {
-  const [todolist, setTodolist] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function getTodo() {
-      await fetch('http://localhost:8080/todos', {
-        method: 'GET',
-        headers: {
-          Authorization: localStorage.getItem('user'),
-        },
-      })
-        .then(res => res.json())
-        .then(data => {
-          setTodolist(data.data);
-        });
-
-      setIsLoading(false);
-    }
-    getTodo();
-  }, []);
-
   return (
     <div className="App">
-      <TodoContext.Provider value={todolist}>
-        {isLoading ? (
-          <></>
-        ) : (
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        )}
-      </TodoContext.Provider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
